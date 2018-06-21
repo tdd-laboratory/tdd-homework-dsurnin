4d79e510a273b3de0f311ea18584e20c278905d7
dsurnin <dmitry.surnin@dsr-corporation.com>
2018-06-21 09:25:50 +0300

    TDD exp
    Signed-off-by: dsurnin <dmitry.surnin@dsr-corporation.com>

--- library_test.py
+++ library_test.py
@@ -30,6 +30,108 @@ class TestCase(unittest.TestCase):
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    def test_extract_date(self):
+        self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, "2015-07-25")
+
+    def test_extract_date_no_date(self):
+        self.assert_extract('I was born on.', library.dates_iso8601)
+
+    def test_extract_date_m_gt_12(self):
+        self.assert_extract('I was born on 2015-13-25.', library.dates_iso8601)
+
+    def test_extract_date_d_gt_31(self):
+        self.assert_extract('I was born on 2015-07-32.', library.dates_iso8601)
+
+    def test_extract_date_short_mon_format(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_short_mon, "25 Jan 2017")
+
+    def test_extract_date_short_mon_format_no_date(self):
+        self.assert_extract('I was born on .', library.dates_short_mon)
+
+    def test_extract_date_short_mon_format_Apr(self):
+        self.assert_extract('I was born on 25 Apr 2017.', library.dates_short_mon, "25 Apr 2017")
+
+    def test_extract_date_short_mon_format_Jan(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_short_mon, "25 Jan 2017")
+
+    def test_extract_date_short_mon_format_Jun(self):
+        self.assert_extract('I was born on 25 Jun 2017.', library.dates_short_mon, "25 Jun 2017")
+
+    def test_extract_date_short_mon_format_Mar(self):
+        self.assert_extract('I was born on 25 Mar 2017.', library.dates_short_mon, "25 Mar 2017")
+
+    def test_extract_date_short_mon_format_May(self):
+        self.assert_extract('I was born on 25 May 2017.', library.dates_short_mon, "25 May 2017")
+
+    def test_iso8601_with_ts_min_w(self):
+        self.assert_extract('I was born on 2018-06-22 18:22. I was born', library.dates_iso8601, "2018-06-22 18:22")
+
+    def test_iso8601_with_ts_min_t(self):
+        self.assert_extract('I was born on 2018-06-22T18:22. I was born', library.dates_iso8601, "2018-06-22T18:22")
+
+    def test_iso8601_with_ts_sec_w(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:11. I was born', library.dates_iso8601, "2018-06-22 18:22:11")
+
+    def test_iso8601_with_ts_sec_t(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:22. I was born', library.dates_iso8601, "2018-06-22T18:22:22")
+
+    def test_iso8601_with_ts_msec_w(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:11.123. I was born', library.dates_iso8601, "2018-06-22 18:22:11.123")
+
+    def test_iso8601_with_ts_msec_t(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:22.123. I was born', library.dates_iso8601, "2018-06-22T18:22:22.123")
+
+    def test_iso8601_with_ts_wrong_h(self):
+        self.assert_extract('I was born on 2018-06-22 30:22:11.123. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_wrong_m(self):
+        self.assert_extract('I was born on 2018-06-22 18:70:22.123. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_wrong_s(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:90.123. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_wrong_ms(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:22.adf. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_min_w_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22 18:22-0800. I was born', library.dates_iso8601, "2018-06-22 18:22-0800")
+
+    def test_iso8601_with_ts_min_t_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22T18:22+0800. I was born', library.dates_iso8601, "2018-06-22T18:22+0200")
+
+    def test_iso8601_with_ts_sec_w_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:11-0800. I was born', library.dates_iso8601, "2018-06-22 18:22:11-0800")
+
+    def test_iso8601_with_ts_sec_t_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:22+0100. I was born', library.dates_iso8601, "2018-06-22T18:22:22+0100")
+
+    def test_iso8601_with_ts_msec_w_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:11.123-0800. I was born', library.dates_iso8601, "2018-06-22 18:22:11.123-0800")
+
+    def test_iso8601_with_ts_msec_t_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22T18:22:22.123+0100. I was born', library.dates_iso8601, "2018-06-22T18:22:22.123+0100")
+
+    def test_iso8601_with_ts_wrong_tz_offset_sep(self):
+        self.assert_extract('I was born on 2018-06-22 10:22:11.123|0800. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_wrong_tz_offset(self):
+        self.assert_extract('I was born on 2018-06-22 18:10:22.123-9999. I was born', library.dates_iso8601)
+
+    def test_iso8601_with_ts_tz_letter(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:10.123UTC. I was born', library.dates_iso8601, "2018-06-22 18:22:10.123UTC")
+
+    def test_iso8601_with_ts_tz_wrong_letter(self):
+        self.assert_extract('I was born on 2018-06-22 18:22:22.123asdf. I was born', library.dates_iso8601)
+
+    def test_extract_date_short_mon_com(self):
+        self.assert_extract('I was born on 25 Jan, 2017.', library.dates_short_mon, "25 Jan, 2017")
+
+    def test_extract_int_com_sep(self):
+        self.assert_extract('I was born on 25,000,000.', library.integers, "25,000,000")
+
+    def test_extract_int_com_sep_wrong(self):
+        self.assert_extract('I was born on 25,0,0,0,000.', library.integers)
+
 
 if __name__ == '__main__':
     unittest.main()
